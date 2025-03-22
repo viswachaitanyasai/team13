61893e3a2bf1858083151ae41d10eb2f35ba0150
Kshitij Gupta <kgkshitij0307@gmail.com>
2025-03-23 01:10:22 +0530

    UI enhancements for auth

--- Frontend/my-vite-app/src/pages/AuthPage.jsx
+++ Frontend/my-vite-app/src/pages/AuthPage.jsx
@@ -38,10 +38,10 @@ const AuthPage = () => {
     setPassword("");
     setConfirmPassword("");
     setFirstName("");
-    setLastName("")
+    setLastName("");
   };
 
-  const handleLogin = async(e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     setErrorMessage("");
 
@@ -79,7 +79,7 @@ const AuthPage = () => {
     }
   };
 
-  const handleRegister = async(e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     setErrorMessage("");
     const trimmedFirstName = firstName.trim();
@@ -88,22 +88,17 @@ const AuthPage = () => {
     const trimmedPassword = password.trim();
     const trimmedConfirmPassword = confirmPassword.trim();
 
-    if (!trimmedFirstName || !trimmedLastName || !trimmedEmail || !trimmedPassword || !trimmedConfirmPassword) {
+    if (!trimmedFirstName || !trimmedEmail || !trimmedPassword) {
       toast.error("All fields are required.");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      toast.error("Passwords do not match!");
       return;
     }
 
     setLoading(true);
     try {
-      const response = await registerUser({ 
-        name: `${trimmedFirstName} ${trimmedLastName}`,
+      const response = await registerUser({
+        name: `${trimmedFirstName}`,
         email: trimmedEmail,
-        password: trimmedPassword
+        password: trimmedPassword,
       });
       toast.success("Registration Successful! Verify your email.");
       console.log(response);
@@ -116,10 +111,10 @@ const AuthPage = () => {
   };
 
   return (
-    <div className="flex h-full items-center justify-center bg-gradient-to-br from-gray-900 to-indigo-900">
-      <div className="flex w-3/4 my-20 bg-gray-800 bg-opacity-90 shadow-2xl rounded-lg overflow-hidden backdrop-blur-md">
+    <div className="flex h-full items-center justify-center bg-black max-h-screen min-h-screen">
+      <div className="flex w-3/4 my-20 bg-gray-900 bg-opacity-90 shadow-2xl rounded-lg overflow-hidden backdrop-blur-md">
         {/* Left Side - Image Section */}
-        <div className="hidden md:flex w-1/2 items-center justify-center p-8">
+        <div className="hidden md:flex w-1/2 items-center justify-center">
           <img
             src="../../src/assets/login.jpg"
             alt="Auth Visual"
@@ -129,20 +124,26 @@ const AuthPage = () => {
 
         {/* Right Side - Form Section */}
         <div className="w-full md:w-1/2 p-10">
-          <h2 className="text-3xl font-bold text-center text-white mb-6">
-            Welcome to EduHack!
-          </h2>
-
           {/* Toggle Buttons */}
-          <div className="flex justify-center mb-6 bg-gray-700 rounded-full p-1">
+          <button
+            className="text-cyan-600 mb-6 self-start hover:text-indigo-300 transition"
+            onClick={() => navigate("/")}
+          >
+            &larr; Back
+          </button>
+          <div className="flex justify-center mb-4 bg-gray-700 rounded-full p-1">
             <button
-              className={`w-1/2 py-2 rounded-full text-lg font-semibold transition-colors duration-300 ${isLogin ? "bg-indigo-600 text-white" : "text-gray-300"}`}
+              className={`w-1/2 py-2 rounded-full text-lg font-semibold transition-colors duration-300 ${
+                isLogin ? "bg-cyan-700 text-white" : "text-gray-300"
+              }`}
               onClick={() => toggleAuthMode(true)}
             >
               Login
             </button>
             <button
-              className={`w-1/2 py-2 rounded-full text-lg font-semibold transition-colors duration-300 ${!isLogin ? "bg-indigo-600 text-white" : "text-gray-300"}`}
+              className={`w-1/2 py-2 rounded-full text-lg font-semibold transition-colors duration-300 ${
+                !isLogin ? "bg-cyan-700 text-white" : "text-gray-300"
+              }`}
               onClick={() => toggleAuthMode(false)}
             >
               Register
@@ -157,115 +158,72 @@ const AuthPage = () => {
               exit={{ x: isLogin ? -100 : 100, opacity: 0 }}
               transition={{ duration: 0.4, ease: "easeInOut" }}
             >
+              <form onSubmit={isLogin ? handleLogin : handleRegister}>
+                {!isLogin && (
+                  <>
+                    {/* First Name */}
+                    <label className="block text-sm ml-2 mb-2 font-medium text-gray-300">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter your Name"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
+                      required
+                    />
 
-
-          <form onSubmit={isLogin ? handleLogin : handleRegister}>
-            {!isLogin && (
-              <>
-                {/* First Name */}
+                    {/* Last Name */}
+                  </>
+                )}
+                {/* Email */}
                 <label className="block text-sm ml-2 mb-2 font-medium text-gray-300">
-                  First Name
+                  Email
                 </label>
                 <input
-                  type="text"
-                  placeholder="Enter your First Name"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
+                  type="email"
+                  placeholder="Enter your Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
                   required
                 />
 
-                {/* Last Name */}
-                <label className="block text-sm ml-2 mb-2 font-medium text-gray-300">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter your Last Name"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
-                  required
-                />
-              </>
-            )}
-            {/* Email */}
-            <label className="block text-sm ml-2 mb-2 font-medium text-gray-300">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="Enter your Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
-              required
-            />
-
-            {/* Password */}
-            <label className="block text-sm font-medium ml-2 mb-2 text-gray-300">
-              Password
-            </label>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter your Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
-                required
-              />
-              <span
-                className="absolute right-3 top-3 cursor-pointer text-gray-400"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </span>
-            </div>
-
-            {/* Confirm Password (only for Register) */}
-            {!isLogin && (
-              <>
+                {/* Password */}
                 <label className="block text-sm font-medium ml-2 mb-2 text-gray-300">
-                  Confirm Password
+                  Password
                 </label>
                 <div className="relative">
                   <input
-                    type={showConfirmPassword ? "text" : "password"}
-                    placeholder="Confirm your Password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     className="w-full px-4 py-2 border rounded-full outline-none mb-3 bg-gray-900 text-white focus:ring-2 focus:ring-indigo-500"
                     required
                   />
                   <span
                     className="absolute right-3 top-3 cursor-pointer text-gray-400"
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                    onClick={() => setShowPassword(!showPassword)}
                   >
-                    {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
                   </span>
                 </div>
-              </>
-            )}
-          {isLogin && (
-            <div className="flex justify-between text-sm text-gray-400 mb-4">
-              <label>
-                <input type="checkbox" className="mr-1" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} /> Remember me
-              </label>
-              <span className="cursor-pointer">Forgot Password?</span>
-            </div>
-            )}
-            <button
-              className={`w-full bg-indigo-600 text-white py-3 rounded-lg transition duration-300 ${
-                loading ? "bg-gray-400 cursor-not-allowed" : "hover:bg-indigo-700"
-              }`}
-              type="submit"
-              disabled={loading}
-            >
-              {loading ? "Processing..." : isLogin ? "Login" : "Register"}
-            </button>
-          </form>
-          </motion.div>
+
+                <button
+                  className={`w-full bg-cyan-700 text-white py-3 rounded-lg transition duration-300 ${
+                    loading
+                      ? "bg-gray-400 cursor-not-allowed"
+                      : "hover:bg-indigo-700"
+                  }`}
+                  type="submit"
+                  disabled={loading}
+                >
+                  {loading ? "Processing..." : isLogin ? "Login" : "Register"}
+                </button>
+              </form>
+            </motion.div>
           </AnimatePresence>
         </div>
       </div>
